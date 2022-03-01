4bc2e4bb7990e8689c6710b77bc07a4ad6a229e7
Martina Fonseca <martina.fonseca@nhsx.nhs.uk>
2022-03-01 13:04:57 +0000

    update approach to authentication

--- git_101.R
+++ git_101.R
@@ -7,10 +7,13 @@ library(usethis)
 usethis::use_git_config(user.name = "Martina Fonseca", user.email = "martina.fonseca@nhsx.nhs.uk")
 # find an access token / generate
 usethis::browse_github_pat()
+#Error: `browse_github_pat()` was deprecated in usethis 2.0.0 and is now defunct.
+#Please use `create_github_token()` instead.
+
+usethis::create_github_token()
 
 # edit r environment, put token in
 usethis::edit_r_environ()
-#GITHUB_PAT=da073f4b9617eee1b112b88089584942f5a277fb
 
 # say you're going to use git
 usethis::use_git()
---
+++ git_101_22.R
@@ -0,0 +1,27 @@
+#https://happygitwithr.com/https-pat.html
+
+
+# install package
+install.packages("usethis")
+library(usethis)
+
+# setup local git as needed without using the shell
+usethis::use_git_config(user.name = "Martina Fonseca", user.email = "martina.fonseca@nhsx.nhs.uk")
+# find an access token / generate
+usethis::browse_github_pat()
+#Error: `browse_github_pat()` was deprecated in usethis 2.0.0 and is now defunct.
+#Please use `create_github_token()` instead.
+
+usethis::create_github_token()
+
+gitcreds::gitcreds_set()
+
+
+# edit r environment, put token in
+#usethis::edit_r_environ()
+
+# say you're going to use git
+usethis::use_git()
+
+# say you're going to use HTTPS protocol
+usethis::use_github(protocol="https")
